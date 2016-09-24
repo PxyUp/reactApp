4ab353156c6a0e20f342e1a1fd916f747c9bd2e2
PyxRu <tduble94@gmail.com>
2016-09-24 11:58:22 +0300

    ReactApp init

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html')
+});
+
+app.get('/js/*.js', function (req, res) {
+    res.sendfile(__dirname + '/public/' + req.url)
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
